177b8e7df7b12cb04a7722edbea20b188579c69e
yuqiaowang <yuqiaowang1993@outlook.com>
2017-07-21 17:53:44 +0800

    add some anotaions

--- src/main/java/SimulationImpl/Tools.java
+++ src/main/java/SimulationImpl/Tools.java
@@ -6,7 +6,7 @@ package SimulationImpl;
 public class Tools {
     /**拓扑相关*/
     //用于设定每个area的负载门限
-    public static double DEFAULTTHRESHOLD = 0.65;
+    public static double DEFAULTTHRESHOLD = 0.6;
     //用于设定每条边的波长数
     public static int DEFAULTNUMBEROFWAVELENTHES = 40;
 
@@ -14,9 +14,9 @@ public class Tools {
     //用于设置生成业务个数
     public static int DEFAULTSERVICENUMBER = 1000;
     //平均业务到达率
-    public static double DEFAULTLAMBDA = 1.33;
+    public static double DEFAULTLAMBDA = 1.538;
     //生成业务时，不同时间段产生的业务源宿点概率不同，用来区分时间
-    public static int DEFAULTWORKINGTIME = 1000;
+    public static int DEFAULTWORKINGTIME = 940;
     //业务平均持续时间
     public static int DEFAULTAVERAGESERVICETIME = 200;
     //单个业务占用最大带宽数
@@ -28,7 +28,7 @@ public class Tools {
 
     /**关于参数的确定*/
     // 平均业务持续时间=200不变
-    // 平均业务到达率 = {2, 1.6, 1.33, 1.14, 1}
+    // 平均业务到达率 = {2, 1.6, 1.33, 1.14, 1} {,1.538,1.66,1.81,2}
     // 业务数量=1000不变
-    // 迁移起始时间 = {1200,1000,800,700,600}
+    // 迁移起始时间 = {1200,1000,800,700,600} {940, ,1120,1200}
 }
