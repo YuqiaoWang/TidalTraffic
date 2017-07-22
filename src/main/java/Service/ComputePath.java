782da52311fd675dbb8ce09292c5ddd7c753f485
yuqiaowang <yuqiaowang1993@outlook.com>
2017-07-22 16:59:10 +0800

    add hop statistic model

--- src/main/java/Service/ComputePath.java
+++ src/main/java/Service/ComputePath.java
@@ -184,18 +184,21 @@ public class ComputePath extends Thread {
                 //各域状态判断
                 areaMapIterator = this.areaHashMap.entrySet().iterator();
                 //把负载值写入文件
-                FileWriter areaOneLoadFileWriter, areaTwoLoadFileWriter, areaThreeLoadFileWriter, totalLoadFileWriter;
+                FileWriter areaOneLoadFileWriter, areaTwoLoadFileWriter, areaThreeLoadFileWriter, totalLoadFileWriter,
+                        hopFileWriter;
 
                 if(Integer.valueOf(service.serviceId) == 0) {
                     areaOneLoadFileWriter = new FileWriter("target/generated-sources/area1.txt", false);
                     areaTwoLoadFileWriter = new FileWriter("target/generated-sources/area2.txt", false);
                     areaThreeLoadFileWriter = new FileWriter("target/generated-sources/area3.txt", false);
                     totalLoadFileWriter = new FileWriter("target/generated-sources/total.txt", false);
+                    hopFileWriter = new FileWriter("target/generated-sources/hop.txt", false);
                 }else {
                     areaOneLoadFileWriter = new FileWriter("target/generated-sources/area1.txt", true);
                     areaTwoLoadFileWriter = new FileWriter("target/generated-sources/area2.txt", true);
                     areaThreeLoadFileWriter = new FileWriter("target/generated-sources/area3.txt", true);
                     totalLoadFileWriter = new FileWriter("target/generated-sources/total.txt", true);
+                    hopFileWriter = new FileWriter("target/generated-sources/hop.txt", true);
                 }
 
                 while (areaMapIterator.hasNext()) {
@@ -249,6 +252,15 @@ public class ComputePath extends Thread {
                 service.setGraphPath(graphPath);
                 System.out.printf("业务 " + service.serviceId + " 已算路: ");
                 List<Vertex> vertexList = graphPath.getVertexList();
+
+                //统计跳数
+                if(nowTime - this.programStartTime > Tools.DEFAULTWORKINGTIME * Tools.TIMESCALE &&
+                        nowTime - this.programStartTime < (Tools.DEFAULTWORKINGTIME + 3 * Tools.DEFAULTAVERAGESERVICETIME) * Tools.TIMESCALE) {
+                    hopFileWriter.write(vertexList.size() - 1 + "\n");
+                    hopFileWriter.close();
+                }
+
+
                 Iterator<Vertex> iterator = vertexList.iterator();
                 while (iterator.hasNext()) {
                     Vertex vertex = iterator.next();
